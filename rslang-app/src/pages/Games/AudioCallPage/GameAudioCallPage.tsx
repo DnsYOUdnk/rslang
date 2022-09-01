efdb53e632a0a8fa95f05065765cbb734ee48bd7
Dinis <d.yudenka@yandex.by>
2022-09-01 15:22:01 +0300

    feat: add a styles for GameAudioCallPage component and implement word reproduction

--- rslang-app/src/pages/Games/AudioCallPage/GameAudioCallPage.tsx
+++ rslang-app/src/pages/Games/AudioCallPage/GameAudioCallPage.tsx
@@ -1,20 +1,40 @@
 import cl from './GameAudioCallPage.module.css';
 import cn from 'classnames';
+import { useEffect, useRef, useState } from 'react';
+import Lottie from 'lottie-react';
 import { GameAudioCallProps } from './GameAudioCallPage.props';
-import { useEffect, useState } from 'react';
 import { CountDown } from '../../../components/CountDown/CountDown';
 import { ButtonSound } from '../../../components/ButtonSound/ButtonSound';
 import { ButtonFullscreen } from '../../../components/ButtonFullScreen/ButtonFullscreen';
 import { Lives } from '../../../components/Lives/Lives';
 import { ButtonClose } from '../../../components/ButtonClose/ButtonClose';
+import soundOn from './../../../assets/json-animation/soundOn.json';
+import { Button } from '../../../components/Button/Button';
+import { Button as Btn } from '@mui/material';
 
 export const GameAudioCallPage = ({ className, ...props }: GameAudioCallProps) => {
   const {words} = props;
   const [startGame, setStartGame] = useState(false);
   const [onSound, setOnSound] = useState(false);
+  const [onPlayWord, setOnPlayWord] = useState(false);
+  const al = useRef(null);
+
+  const audioPlayer = new Audio();
+
+  const playAudioWord = (): void => {
+    setOnPlayWord(!onPlayWord)
+    audioPlayer.src = 'https://react-learn-language.herokuapp.com/files/01_0018.mp3';
+    audioPlayer.play();
+  }
+
+  audioPlayer.addEventListener('ended',() => {
+    setOnPlayWord(false)
+  })
 
   const countDownHandler = (start: boolean): void => {
-    setStartGame(start)
+    setStartGame(start);
+    setOnPlayWord(start);
+    playAudioWord()
   }
 
   const handlerSoundChange = () => {
@@ -34,17 +54,28 @@ export const GameAudioCallPage = ({ className, ...props }: GameAudioCallProps) =
           <ButtonClose/>
         </div>
       </div>
-      <div className="audiocall__container">
-        <div className="audiocall__learn_word"></div>
-        <ul className="audiocall__translation_words">
-          <li className="audiocall__transition__word"></li>
-          <li className="audiocall__transition__word"></li>
-          <li className="audiocall__transition__word"></li>
-          <li className="audiocall__transition__word"></li>
-          <li className="audiocall__transition__word"></li>
+      <div className={cn(cl.container)}>
+        <div 
+          className={onPlayWord ? cn(cl.learn_word, cl.enable_play) : cn(cl.learn_word)} 
+          onClick={() => !onPlayWord && playAudioWord()}
+        >
+          <Lottie
+            className={cn(className, cl.word_player)}
+            animationData={soundOn}
+            loop={onPlayWord}
+            autoplay={onPlayWord}
+            title={'Воспроизвести слово'}
+          />
+        </div>
+        <ul ref={al} className={cn(className, cl.translation_words)} onClick= {() => console.log(words)}>
+          <li className="audiocall__transition__word"><Button className={cn(cl.button__translation_word)}>Не знаю</Button></li>
+          <li className="audiocall__transition__word"><Button className={cn(cl.button__translation_word)}>Не знаю</Button></li>
+          <li className="audiocall__transition__word"><Button className={cn(cl.button__translation_word)}>Не знаю</Button></li>
+          <li className="audiocall__transition__word"><Button className={cn(cl.button__translation_word)}>Не знаю</Button></li>
+          <li className="audiocall__transition__word"><Button className={cn(cl.button__translation_word)}>Не знаю</Button></li>
         </ul>
         <div className="audiocall__button">
-          <button className='button__next'></button>
+          <Button className={cn(cl.button__next)}>Не знаю</Button>
         </div>
       </div>
     </div>
