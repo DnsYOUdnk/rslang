23ee28e2579d0d88fe2afa7efc9b358efd207724
Dinis <d.yudenka@yandex.by>
2022-09-04 13:39:58 +0300

    fix: edits according to comments

--- rslang-app/src/components/Layout/Layout.tsx
+++ rslang-app/src/components/Layout/Layout.tsx
@@ -1,14 +1,19 @@
-import { Outlet } from 'react-router-dom';
-import { Footer } from '../Footer/Footer';
-import { Header } from '../Header/Header';
-import cl from './Layout.module.css';
-
-export const Layout = (): JSX.Element => {
-  return (
-    <div className={cl.wrapper}>
-      <Header className={cl.header}></Header>
-      <Outlet />
-      <Footer className={cl.footer}></Footer>
-    </div>
-  );
+import cn from 'classnames'; 
+import { Outlet, useMatch } from 'react-router-dom'; 
+import { Footer } from '../Footer/Footer'; 
+import { Header } from '../Header/Header'; 
+import cl from './Layout.module.css'; 
+ 
+export const Layout = (): JSX.Element => { 
+  const audio = useMatch('/games/audiocall'); 
+  const sprint = useMatch('/games/sprint'); 
+  return ( 
+    <div className={cl.wrapper}> 
+      <Header className={cn(cl.header, { 
+        [cl.game_header] : audio || sprint 
+      })}></Header> 
+      <Outlet /> 
+      {!(audio || sprint) && <Footer className={cl.footer}></Footer>} 
+    </div> 
+  ); 
 };
--- rslang-app/src/pages/GameWrapperPage/GameWrapperPage.tsx
+++ rslang-app/src/pages/GameWrapperPage/GameWrapperPage.tsx
@@ -1,73 +1,71 @@
-import cl from './GameWrapperPage.module.css';
-import cn from 'classnames';
-import React, { cloneElement, useState } from 'react';
-import { GameWrapperPageProps } from './GameWrapperPage.props';
-import { GameModalWindow } from '../../components/GameModalWindow/GameModalWindow';
-import { Header } from '../../components/Header/Header';
-import { LevelGroupWords } from '../../components/LevelGroupWords/LevelGroupWords';
-import { useGetWords } from '../../customHooks/useGetWords';
-import { GameLoader } from '../../components/GameLoader/GameLoader';
-import { IWord } from '../../types/dataWordTypes';
-import { GameStatistic } from '../../components/GameStatistic/GameStatistic';
-
-export const GameWrapperPage = ({
-  dataGame,
-  children,
-  className,
-  ...props
-}: GameWrapperPageProps): JSX.Element => {
-  const { onLoading, listWords, getWords } = useGetWords();
-  const [resultWordsArr, setResultWordsArr] = useState<IWord[]>([]);
-  const [onStart, setOnStart] = useState(false);
-  const [endGame, setEndGame] = useState(false);
-
-  const levelHandler = (group: number): void => {
-    getWords(group);
-    setOnStart(true);
-  };
-
-  const repeatGame = () => {
-    setResultWordsArr([]);
-    setEndGame(false);
-    setOnStart(true);
-  };
-
-  const gameContent = (() => {
-    if (children && !onLoading && onStart && listWords.length) {
-      const gameProps = {
-        words: listWords,
-        quantityWords: listWords.length,
-        setEndGame: setEndGame,
-        resultWordsArr: resultWordsArr,
-        setResultWordsArr: setResultWordsArr,
-      };
-      return cloneElement(children as React.ReactElement, gameProps);
-    }
-    return null;
-  })();
-
-  return (
-    <>
-      <Header className={cn(cl.game_header)} />
-      <main className={cn(className, cl.main)}>
-        {(() => {
-          if (!endGame) {
-            if (onStart && listWords.length) {
-              return <>{gameContent}</>;
-            } else {
-              return !onLoading ? (
-                <GameModalWindow className={cn(cl.modal_window)} dataGame={dataGame}>
-                  <LevelGroupWords levelHandler={levelHandler} />
-                </GameModalWindow>
-              ) : (
-                <GameLoader />
-              );
-            }
-          } else {
-            return <GameStatistic resultWordsArr={resultWordsArr} repeatGame={repeatGame} />;
-          }
-        })()}
-      </main>
-    </>
-  );
+import cl from './GameWrapperPage.module.css'; 
+import cn from 'classnames'; 
+import React, { cloneElement, useState } from 'react'; 
+import { GameWrapperPageProps } from './GameWrapperPage.props'; 
+import { GameModalWindow } from '../../components/GameModalWindow/GameModalWindow'; 
+import { LevelGroupWords } from '../../components/LevelGroupWords/LevelGroupWords'; 
+import { useGetWords } from '../../customHooks/useGetWords'; 
+import { GameLoader } from '../../components/GameLoader/GameLoader'; 
+import { IWord } from '../../types/dataWordTypes'; 
+import { GameStatistic } from '../../components/GameStatistic/GameStatistic'; 
+ 
+export const GameWrapperPage = ({ 
+  dataGame, 
+  children, 
+  className, 
+  ...props 
+}: GameWrapperPageProps): JSX.Element => { 
+  const { onLoading, listWords, getWords } = useGetWords(); 
+  const [resultWordsArr, setResultWordsArr] = useState<IWord[]>([]); 
+  const [onStart, setOnStart] = useState(false); 
+  const [endGame, setEndGame] = useState(false); 
+ 
+  const levelHandler = (group: number): void => { 
+    getWords(group); 
+    setOnStart(true); 
+  }; 
+ 
+  const repeatGame = () => { 
+    setResultWordsArr([]); 
+    setEndGame(false); 
+    setOnStart(true); 
+  }; 
+ 
+  const gameContent = (() => { 
+    if (children && !onLoading && onStart && listWords.length) { 
+      const gameProps = { 
+        words: listWords, 
+        quantityWords: listWords.length, 
+        setEndGame: setEndGame, 
+        resultWordsArr: resultWordsArr, 
+        setResultWordsArr: setResultWordsArr, 
+      }; 
+      return cloneElement(children as React.ReactElement, gameProps); 
+    } 
+    return null; 
+  })(); 
+ 
+  return ( 
+    <> 
+      <main className={cn(className, cl.main)}> 
+        {(() => { 
+          if (!endGame) { 
+            if (onStart && listWords.length) { 
+              return <>{gameContent}</>; 
+            } else { 
+              return !onLoading ? ( 
+                <GameModalWindow  dataGame={dataGame}> 
+                  <LevelGroupWords levelHandler={levelHandler} /> 
+                </GameModalWindow> 
+              ) : ( 
+                <GameLoader /> 
+              ); 
+            } 
+          } else { 
+            return <GameStatistic resultWordsArr={resultWordsArr} repeatGame={repeatGame} />; 
+          } 
+        })()} 
+      </main> 
+    </> 
+  ); 
 };
